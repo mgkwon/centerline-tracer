3d5f44a44c3737cb4134b06a1ae7e669ac4d21d8
Mingu Kwon <mgskwon@gmail.com>
2021-03-17 18:34:00 +0100

    fix

--- centerline-tracer/centerline-tracer.py
+++ centerline-tracer/centerline-tracer.py
@@ -2,20 +2,28 @@ import numpy as np
 import random
 import cv2
 
-def show(img):
-    cv2.imshow("ha", img)
+
+def show(img, name=""):
+    cv2.imshow("image", img)
+    cv2.imwrite(name+".png", img)
     cv2.waitKey(0)
 
-def filter_neighbors(ns):    
+
+def invert(img):
+    img = (255-img)
+
+
+def filter_neighbors(ns):
     i = 0
     while i < len(ns):
         j = i + 1
         while j < len(ns):
             if (ns[i][0] == ns[j][0] and abs(ns[i][1] - ns[j][1]) <= 1) or (ns[i][1] == ns[j][1] and abs(ns[i][0] - ns[j][0]) <= 1):
                 del ns[j]
-                break                                    
+                break
             j += 1
-        i += 1    
+        i += 1
+
 
 def sort_points_types(pnts, img):
     extremes = []
@@ -26,14 +34,22 @@ def sort_points_types(pnts, img):
         x = p[0]
         y = p[1]
         n = []
-        if img[y - 1,x] > 0: n.append((y-1, x))
-        if img[y - 1,x - 1] > 0: n.append((y-1, x - 1))
-        if img[y - 1,x + 1] > 0: n.append((y-1, x + 1))
-        if img[y,x - 1] > 0: n.append((y, x - 1))
-        if img[y,x + 1] > 0: n.append((y, x + 1))
-        if img[y + 1,x] > 0: n.append((y+1, x))
-        if img[y + 1,x - 1] > 0: n.append((y+1, x - 1))
-        if img[y + 1,x + 1] > 0: n.append((y+1, x + 1))
+        if img[y - 1, x] > 0:
+            n.append((y-1, x))
+        if img[y - 1, x - 1] > 0:
+            n.append((y-1, x - 1))
+        if img[y - 1, x + 1] > 0:
+            n.append((y-1, x + 1))
+        if img[y, x - 1] > 0:
+            n.append((y, x - 1))
+        if img[y, x + 1] > 0:
+            n.append((y, x + 1))
+        if img[y + 1, x] > 0:
+            n.append((y+1, x))
+        if img[y + 1, x - 1] > 0:
+            n.append((y+1, x - 1))
+        if img[y + 1, x + 1] > 0:
+            n.append((y+1, x + 1))
         filter_neighbors(n)
         if len(n) == 1:
             extremes.append(p)
@@ -44,11 +60,16 @@ def sort_points_types(pnts, img):
     return extremes, connections, simple
 
 
-img_path = "test.png"
+img_path = "4.png"
 
 img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 
-_, th = cv2.threshold(img, 128, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)
+# invert(img)
+# img = (255-img)
+# cv2.blur(img, (5, 5))
+
+_, th = cv2.threshold(img, 150, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)
+show(th, 'threashold')
 thin = cv2.ximgproc.thinning(th)
 use_cv2 = False
 
@@ -59,43 +80,55 @@ if use_cv2:
 
     colored = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
-    i = 1 # skip label 0 as it corresponds to the backgground points
-    sum_of_cc_points = 0 
+    i = 1  # skip label 0 as it corresponds to the backgground points
+    sum_of_cc_points = 0
     while i < no_ccs:
-        label_pnts_dic[i] = np.where(labels == i) #where return tuple(list of x cords, list of y cords)
-        colored[label_pnts_dic[i]] = (random.randint(100, 255), random.randint(100, 255), random.randint(100, 255))
+        # where return tuple(list of x cords, list of y cords)
+        label_pnts_dic[i] = np.where(labels == i)
+        colored[label_pnts_dic[i]] = (random.randint(
+            100, 255), random.randint(100, 255), random.randint(100, 255))
         i += 1
 
     # for label in label_pnts_dic:
     #     for p in range(0,len(label_pnts_dic[label][0])):
     #         cv2.circle(colored, (label_pnts_dic[label][1][p], label_pnts_dic[label][0][p]), 2, 128)
     #         show(colored)
-        
+
 else:
     no_ccs, labels = cv2.connectedComponents(thin)
     label_pnts_dic = {}
-    # colored = cv2.cvtColor(thin, cv2.COLOR_GRAY2BGR)
-
     copy = np.zeros(thin.shape, np.uint8)
+    colored = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
-    i = 1 # skip label 0 as it corresponds to the backgground points
-    sum_of_cc_points = 0 
+    i = 1
+    sum_of_cc_points = 0
     while i < no_ccs:
-        label_pnts_dic[i] = np.where(labels == i) #where return tuple(list of x cords, list of y cords)
+        label_pnts_dic[i] = np.where(labels == i)
         copy[label_pnts_dic[i]] = 255
         i += 1
+
+    show(copy, 'thin')
+
+    i = 1
+    sum_of_cc_points = 0
+    while i < no_ccs:
+        label_pnts_dic[i] = np.where(labels == i)
+        colored[label_pnts_dic[i]] = 255
+        i += 1
+
+    show(colored, 'thin_col')
 
     pnts = cv2.findNonZero(copy)
     pnts = np.squeeze(pnts)
 
     ext, conn, simple = sort_points_types(pnts, copy)
 
-    colored = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-
     for p in conn:
-        cv2.circle(colored, (p[0], p[1]), 5, (random.randint(100, 255), random.randint(100, 255), random.randint(100, 255)))
+        cv2.circle(colored, (p[0], p[1]), 3, (124, 134, 240))
+
+        # random.randint(100, 255), random.randint(100, 255), random.randint(100, 255)
 
     for p in ext:
-        cv2.circle(colored, (p[0], p[1]), 5, (random.randint(100, 255), random.randint(100, 255), random.randint(100, 255)))
-    
-    show(colored)
+        cv2.circle(colored, (p[0], p[1]), 3, (123, 232, 133))
+
+    show(colored, 'thin_col_pts')
